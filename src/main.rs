61ab60bc869610e5f1b333e1e21839da2f32f3cc
SharK-020 <dev.kunalshar0@gmail.com>
2025-02-28 21:14:14 +0000

    finished guessing game

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use rand::Rng;
 fn main(){
   println!("Guess the number");
 
+  let secret_number= rand::thread_rng().gen_range(1..=100);
 
   loop{
-    let secret_number= rand::thread_rng().gen_range(1..=100);
+
     println!("Please input your guess.");
 
     let mut guess = String::new();
@@ -16,16 +17,21 @@ fn main(){
       .expect("Failed to read line");
 
 
-    let guess:u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse(){
+        Ok(num)=>num,
+        Err(_)=>continue,
+    };
 
     println!("You guessed: {guess}");
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-  }
+        Ordering::Equal => {
+          println!("You win!");
+          break;
+        },
+    }
 
-    println!("Secret Number: {secret_number}");
   }
 }
